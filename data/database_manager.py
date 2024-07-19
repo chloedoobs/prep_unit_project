5e3912a362e2d71302cd6c8e267a2247e44331a2
Chloe Doobay <chloedoobay@Chloes-MacBook-Pro.local>
2024-07-19 16:30:34 +0100

    commit msg

--- data/database_manager.py
+++ data/database_manager.py
@@ -6,12 +6,13 @@ def create_connection(db_file):
     """ create a database connection to a SQLite database """
     conn = None
     try:
-        conn = None  # To-Do add a connection for the database
+        conn = sqlite3.connect(db_file)  # To-Do add a connection for the database
+        print(f"Connection to {db_file} established successfully.")
     except Error as e:
         print(e)
-
     #To-Do return the connection 
-    return None 
+    return conn
+
 
 def close_connection(conn):
     """ closes a connection to a database """
@@ -20,12 +21,13 @@ def close_connection(conn):
 
 def select_all(conn):
     """select all rows from our table using the conn we already created """
-    cur = conn.cursor()
-    query = "" # To-Do write the query to retrive all data from the longley table 
-
+    cur = conn.cursor() #issue with cursor being initiated?
+    query = "SELECT * FROM longley"
+    
+    # Fetch all rows from the cursor
     cur.execute(query)
 
-    rows = None  # To-Do fetch all rows using the cursor cur
+    rows = cur.fetchall()  # To-Do fetch all rows using the cursor cur
 
     return rows 
 
